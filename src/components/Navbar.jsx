7e500f113b4bf9415d0b41de5a915982f9bb4e5a
villahermosas3 <villahermosas3@berea.edu>
2023-10-19 01:46:00 -0400

    Fixed resume link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -150,7 +150,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='smitchelvi@gmail.com'
+              href='mailto:smitchelvi@gmail.com'
             >
               Email <HiOutlineMail size={30} />
             </a>
@@ -158,7 +158,9 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='resume.pdf'
+              href={resume}
+              target="_blank"
+              rel="noopener noreferrer"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
