4a0a1482cab06b571e93a85eb76d25e9fe26e7c0
Samvae <97714804+Samvae@users.noreply.github.com>
2025-07-07 18:11:52 -0400

    updated experience

--- src/components/Experiences.jsx
+++ src/components/Experiences.jsx
@@ -13,24 +13,54 @@ const Experiences = () => {
         </div>
         {/* container for projects */}
         <div className='w-full flex flex-col justify-center gap-8 text-left'>
-          {/* IT Technician */}
+            {/* Web/Database Analyst */}
           <div className='hover:scale-110 duration-500'>
             <h1 className='text-lg font-bold text-[#074276 py-2 px-4'>
-              January 2023 - Present
+              January 2024 - May 2025
             </h1>
             <h2 className='text-sm font-bold border-b-4 text-[#000000] py-2 px-4'>
-              IT Technician, IT Department
+              Web/Database Analyst, Information Technology Department
             </h2>
             <p className='text-sm text-[#000000] py-2 px-4'>
-              • Delivered technical support to over 500 individuals, including students, faculty, and staff, addressing a wide range of computer-related issues. <br />
-              • Diagnosed and resolved hardware and software problems on more than 100 devices, ensuring seamless user experiences. <br />
-              • Executed over 50 system upgrades and installations, enhancing functionality and bolstering security measures.
+              • Developed and deployed custom full-stack web applications integrated with secure PostgreSQL databases, supporting over 2,300 campus users. <br />
+              • Maintained and queried databases with sensitive records, enabling internal tools and web-based reporting systems. <br />
+              • Led backend development for the Student Government Association voting platform, ensuring secure elections. <br />
+              • Coordinated Agile team sprints using Jira and improved process documentation via internal Wiki updates.
+            </p>
+          </div>
+          Coordinated Agile team sprints using Jira and improved process documentation via internal Wiki updates.
+          {/*Undergraduate Research Assistant*/}
+          <div className='hover:scale-110 duration-500'>
+            <h1 className='text-lg font-bold text-[#074276 py-2 px-4'>
+              May 2024 - July 2024
+            </h1>
+            <h2 className='text-sm font-bold border-b-4 text-black-300 py-2 px-4'>
+              Undergraduate Research Assistant, URCPP Program
+            </h2>
+            <p className='text-sm text-[#000000] py-4 px-4'>
+              • Built image processing algorithms using OpenCV to detect and count stars from 1,000+ NASA images.<br />
+              • Applied parallel processing techniques in Python to improve runtime efficiency by 70% for image classification.<br />
+              • Engineered reusable data pipelines to clean and analyze large scientific image sets for statistical modeling.
+            </p>
+          </div>
+          {/*Information Technology Technician*/}
+          <div className='hover:scale-110 duration-500'>
+            <h1 className='text-lg font-bold text-[#074276 py-2 px-4'>
+              January 2023 - January 2024
+            </h1>
+            <h2 className='text-sm font-bold border-b-4 text-black-300 py-2 px-4'>
+              Information Technology Technician, Information Technology Department
+            </h2>
+            <p className='text-sm text-[#000000] py-4 px-4'>
+              • Resolved hardware, software, and network issues for over 1000+ faculty, staff, and student devices.<br />
+              • Maintained user accounts, access controls, and authentication using Active Directory, enhancing system security.<br />
+              • Mentored incoming IT technicians and created standardized troubleshooting documentation, improving team onboarding and service consistency.
             </p>
           </div>
           {/*Woodshop Worker*/}
           <div className='hover:scale-110 duration-500'>
             <h1 className='text-lg font-bold text-[#074276 py-2 px-4'>
-              August 2022 - December 2022
+              August 2022 - Jan 2023
             </h1>
             <h2 className='text-sm font-bold border-b-4 text-black-300 py-2 px-4'>
               Woodshop Worker, Arts and Crafts
@@ -40,6 +70,7 @@ const Experiences = () => {
               • Utilized woodworking skills and consistent use of woodworking machinery to craft new wood pieces, producing 50+ pieces of high-quality furniture.<br />
             </p>
           </div>
+          
           {/* Complex Assistant */}
           <div className='hover:scale-110 duration-500'>
             <h1 className='text-lg font-bold text-[#074276 py-2 px-4'>
